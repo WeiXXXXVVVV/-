76e4e1e99fa3be1bd476709ca96bb6a9821ed3e9
wei <89676348+WeiXXXXVVVV@users.noreply.github.com>
2022-10-20 23:55:28 +0800

    Create g.c

---
+++ g.c
@@ -0,0 +1,80 @@
+#include<stdio.h>
+#define A 4.0 
+#define B 3.0
+#define C 2.0 
+#define F 0 
+int main () {
+    char ch;
+    int a=0,b=0;
+    float c=0,d=0,f=0;
+    while ((ch = getchar())!='\n'){
+        if(ch>='0' && ch<='9'){
+            if(ch = '0'){                
+               b = 0;
+               a= a+b;
+            }
+            else if(ch == '1'){                
+               b = 1;
+               a= a+b;
+            }
+            else if(ch == '2'){                
+               b = 2;
+                a= a+b;
+            }
+            else if(ch == '3'){                
+               b = 3;
+                a= a+b;
+            }
+            else if(ch == '4'){                
+               b = 4;
+                a= a+b;
+            }
+            else if(ch == '5'){                
+               b = 5;
+                a= a+b;
+            }
+            else if(ch == '6'){                
+               b = 6;
+                a= a+b;
+            }
+            else if(ch == '7'){                
+               b = 7;
+                a= a+b;
+            }
+            else if(ch == '8'){                
+               b = 8;
+                a= a+b;
+            }
+            else if(ch == '9'){                
+               b = 9;
+                a= a+b;
+            }
+        }
+        else if(ch>='A' && ch<='F'){
+            if(ch = 'A'){                
+               d = b * A;
+            }
+             else if(ch == 'B'){                
+               d = b * B;
+            }
+             else if(ch == 'C'){                
+               d = b * C;
+            }
+             else if(ch == 'F'){                
+               d = b * F;
+            }
+            
+        }
+        else if(ch=='+'){
+            d = 0.3 *b;
+        }
+        else if(ch=='-'){
+            d  = (-0.3)*b;
+        }
+        f= f+d;                         
+    }
+    c= f / a; 
+    printf("%d",a);
+    printf("%.2f",c);
+    return 0;
+}
